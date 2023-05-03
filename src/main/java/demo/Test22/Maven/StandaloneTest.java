da759af10832c1fbfe5b486036c350227e633785
Ashok_Nagappan <jjancychristy1988@gmail.com>
2023-05-03 16:49:30 -0400

    by Y

--- src/main/java/demo/Test22/Maven/StandaloneTest.java
+++ src/main/java/demo/Test22/Maven/StandaloneTest.java
@@ -4,7 +4,9 @@ public class StandaloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println( "Hello World!" );
+		System.out.println( "Hello World2222" );
+		System.out.println( "Hello World3333m" );
 	}
 
 }
